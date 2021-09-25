d76b1faa848585ed6056b135081e47f988c06a82
fish895623 <34264960+fish895623@users.noreply.github.com>
2021-09-26 00:39:03 +0900

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -8,6 +8,6 @@ sed -i '/^\[options]$/a\ParallelDownloads=10' /etc/pacman.conf
 pacstrap /mnt \
   base base-devel linux linux-firmware neovim networkmanager \
   git man-db man-pages texinfo dosfstools e2fsprogs mdadm \
-  curl zsh
+  curl zsh 
   
 genfstab -U /mnt >> /mnt/etc/fstab
